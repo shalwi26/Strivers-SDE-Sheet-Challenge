2e07743150ef89a706adf6ff57f2e5b649c3237c
Shalwi Kumari <98161297+shalwi26@users.noreply.github.com>
2023-06-30 16:07:16 +0530

    day18

---
+++ Longest common prefix.cpp
@@ -0,0 +1,23 @@
+string longestCommonPrefix(vector<string> &arr, int n)
+{
+    // Approach Using Normal Iterative Approach
+    string ans;
+    for(int i = 0; i < arr[0].length(); i++){
+        char curr = arr[0][i];
+        bool match = true;
+        for(int j = 1; j < n; j++){
+            if(!(arr[j].length() > i && arr[j][i] == curr)){
+                match = false;
+                break;
+            }
+        }
+        if(match){
+            ans.push_back(curr);
+        }
+        else{
+            break;
+        }
+    }
+
+    return ans;
+}
