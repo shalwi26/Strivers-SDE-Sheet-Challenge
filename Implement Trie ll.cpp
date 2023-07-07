5b3f9bb51847c2b0e8dd26edbc19cd1dcff4eb45
Shalwi Kumari <98161297+shalwi26@users.noreply.github.com>
2023-07-07 19:33:35 +0530

    day24

---
+++ Implement Trie ll.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h> 
+
+struct Node {
+  Node *links[26];
+    int cntEndsWith = 0, cntPrefix = 0;
+
+      void put(char ch, Node *newNode) { links[ch - 'a'] = newNode; }
+
+        Node *get(char ch) { return links[ch - 'a']; }
+
+          bool containsKey(char ch) { return links[ch - 'a'] != NULL; }
+
+            void increaseEnd() { cntEndsWith++; }
+
+              void increasePrefix() { cntPrefix++; }
+
+                void decreaseEnd() { cntEndsWith--; }
+
+                  void decreasePrefix() { cntPrefix--; }
+
+                    int getEnd() { return cntEndsWith; }
+
+                      int getPrefixes() { return cntPrefix; }
+                      };
+                      
+class Trie{
+    Node *root;
+    public:
+
+    Trie(){
+        root = new Node();// Write your code here.
+    }
+
+    void insert(string &word){
+        Node *node = root;
+            for (auto c : word) {
+                  if (!node->containsKey(c))
+                          node->put(c, new Node());
+                          
+                                node = node->get(c);
+                                      node->increasePrefix();
+                                          }
+                                        node->increaseEnd();  // Write your code here.
+    }
+
+    int countWordsEqualTo(string &word){
+        Node *node = root;
+            for (auto c : word) {
+                  if (!node->containsKey(c))
+                          return 0;
+                                else
+                                        node = node->get(c);
+                                            }
+                                                return node->getEnd();// Write your code here.
+    }
+
+    int countWordsStartingWith(string &word){
+        Node *node = root;
+            for (auto c : word) {
+                  if (!node->containsKey(c))
+                          return 0;
+                                else
+                                        node = node->get(c);
+                                            }
+                                                return node->getPrefixes();// Write your code here.
+    }
+
+    void erase(string &word){
+        Node *node = root;
+            for (auto c : word) {
+                  if (!node->containsKey(c))
+                          return;
+                          
+                                node = node->get(c);
+                                      node->decreasePrefix();
+                                          }
+                                              
+                                                  node->decreaseEnd();// Write your code here.
+    }
+};
